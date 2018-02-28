fb3e95e9314671e50f2a2d4997c0a323c0e5bc93
an2n <an2n@DESKTOP-PPKS5BV>
2018-02-28 15:10:11 +0100

    oppdatert interface

--- DAL/ITextMiningDAL.cs
+++ DAL/ITextMiningDAL.cs
@@ -8,11 +8,10 @@ namespace DAL
 {
     interface ITextMiningDAL
     {
-
-       
-       
         List<string> getCristinID();
+
         List<string> getTitles(string cristinID);
+
         List<List<string>> tokenizeTitles(List<string> titles);
 
         List<List<string>> removeStopWords(List<List<string>> tokenizedTitles);
@@ -24,14 +23,26 @@ namespace DAL
 
         List<List<string>> stemTitles(List<List<string>> tokenizedTitles);
 
+        List<string> groupTitles(List<List<string>> tokenizedTitles);
+
+        /*
+         * 1. Skal lagre alle nye ord i CloudWord tabellen og oppdatere antall
+         * Eksempel: key | word | count
+         * 2. Skal lage en ny kolonne med personen sine CloudWords og antall
+         * Eksempel: cristinID | key1 | antall1 | key2 | antall2 | key3 | antall3
+         */
+        bool saveWordCloud(List<string> groupedTitles);
+
+        List<string> getTopCloudWords();
+
+        bool deleteWordCloudTable();
+
+        bool createWordCloudTable();
 
         // (
         // 1 (1 (frode) 2 (er) 3(best)
         // 2 (1 (ok) 2 (ja) 3 (nei))
         // 3 ( 1 () 2 () 3 ())
         // )
-
-
-
     }
 }
