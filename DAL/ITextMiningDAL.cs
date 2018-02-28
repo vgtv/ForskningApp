177c56a4a03b90fa3097d511e5712135dd30de0e
an2n <an2n@DESKTOP-PPKS5BV>
2018-02-28 13:56:31 +0100

    halveis commit

---
+++ DAL/ITextMiningDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    interface ITextMiningDAL
+    {
+
+       
+       
+        List<string> getCristinID();
+        List<string> getTitles(string cristinID);
+        List<List<string>> tokenizeTitles(List<string> titles);
+
+        List<List<string>> removeStopWords(List<List<string>> tokenizedTitles);
+        bool checkStopWords(List<string> tokenizedTitle);
+
+        // Debug.WriteLine( slettet tittel )
+        List<List<string>> removeLanguages(List<List<string>> tokenizedTitles);
+        bool checkLanguage(List<string> title);
+
+        List<List<string>> stemTitles(List<List<string>> tokenizedTitles);
+
+
+        // (
+        // 1 (1 (frode) 2 (er) 3(best)
+        // 2 (1 (ok) 2 (ja) 3 (nei))
+        // 3 ( 1 () 2 () 3 ())
+        // )
+
+
+
+    }
+}
